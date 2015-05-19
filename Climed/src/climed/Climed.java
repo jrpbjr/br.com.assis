7602cb8b0de121e4e2186298040eef9c77a3c269
jr <jr@192.168.1.112>
2015-05-19 16:25:45 -0300

    Pacote principal, chamando tela inicial

--- Climed/src/climed/Climed.java
+++ Climed/src/climed/Climed.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package climed;
+import View.ClimedMDIApplication;
+import javax.swing.JFrame;
+
 
 /**
  *
@@ -16,6 +19,10 @@ public class Climed {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ClimedMDIApplication frm=new ClimedMDIApplication();
+        frm.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frm.setVisible(true);
+        
     }
     
 }
